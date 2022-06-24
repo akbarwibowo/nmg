c4c5992c0f3fe52235851693c01207f87670e903
akbarwibowo <91762971+akbarwibowo@users.noreply.github.com>
2022-06-24 23:39:57 +0700

    Add files via upload

---
+++ forms/admin_order.php
@@ -0,0 +1,12 @@
+<?php
+
+include "connect_order.php";
+
+$nama = $_POST['name'];
+$alamat = $_POST['address'];
+$jenis = $_POST['subject'];
+$jumlah = $_POST['message'];
+
+mysqli_query($connect_order, "INSERT INTO order_data(nama, alamat, jenis, jumlah) VALUES('$nama', '$alamat', '$jenis', '$jumlah');");
+
+header("Location: ../../admin.php");
